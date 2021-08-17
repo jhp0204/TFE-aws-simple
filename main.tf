d41561c5f01a2f078cf10984286a1bd1f7a1a251
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-08-17 11:06:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "aws" {
-  version = "2.33.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
-  region = "us-east-2"
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
 
 provider "random" {

4cf5afede15758be473583f3e9cf9391f0eccf5d
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-08-17 11:53:26 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.27"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
+  profile = "default"
   region  = "us-east-2"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
